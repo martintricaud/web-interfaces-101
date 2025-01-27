2364475e50806ebdc303180659201b8706ec6ea6
Martin Tricaud <martin.tricaud@gmail.com>
2025-01-27 14:05:29 +0100

    fixes imports for highlight.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import css from 'highlight.js/lib/languages/css';
+import html from 'highlight.js/lib/languages/xml'; // HTML is handled by the XML language in highlight.js
+import 'highlight.js/styles/github.css';
+// Register the languages you want to highlight
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('html', html);
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+      // document.querySelectorAll('code').forEach((el) => {
+      //     hljs.highlightElement(el);
+      // });
+      hljs.highlightAll();
+  });
+
 // document.querySelector('#app').innerHTML = `
 //   <div>
 //     <a href="https://vite.dev" target="_blank">
