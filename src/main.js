a086fc61cc746d4b221c995abdb29a0b955a52e2
Martin Tricaud <martin.tricaud@gmail.com>
2025-01-27 16:31:21 +0100

    updates course content

--- src/main.js
+++ src/main.js
@@ -1,42 +1,18 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import css from 'highlight.js/lib/languages/css';
-import html from 'highlight.js/lib/languages/xml'; // HTML is handled by the XML language in highlight.js
+import html from 'highlight.js/lib/languages/xml'; 
 import 'highlight.js/styles/github.css';
-// Register the languages you want to highlight
+
+// Register the languages that will be highlighted
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('css', css);
 hljs.registerLanguage('html', html);
 
 
 document.addEventListener('DOMContentLoaded', (event) => {
-      // document.querySelectorAll('code').forEach((el) => {
-      //     hljs.highlightElement(el);
-      // });
       hljs.highlightAll();
   });
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
 
-// setupCounter(document.querySelector('#counter'))
